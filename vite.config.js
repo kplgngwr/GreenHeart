c1a26aa6cfd52c1b082719cc580e9ed7af52500a
Kapil <kapilgangwar2003@gmail.com>
2025-03-08 16:52:16 +0530

    Updated with Vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   build: {
     outDir: 'build',
     chunkSizeWarningLimit: 1000, // Increase from default 500
